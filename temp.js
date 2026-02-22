44b2cf41039a43f5ffc4d6af52c0b40d43db41ef
AbhishekMule13 <abhimule1308@gmail.com>
2026-02-22 15:04:43 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -2,9 +2,13 @@
 // last="Mule";
 // // console.log(first+" "+last);
 
+// const city="Pune";
+// const city1="mumbai";
+// console.log(city1.toUpperCase());
 
-
-
-
+fruits=["apple","banana","grapes"];
+console.log(fruits.length)
+fruit=fruits[1]
+console.log(fruit)
 
 
